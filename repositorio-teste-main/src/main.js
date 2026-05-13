9b32adbc477eb939d3fb919ff3e82947309e80dd
Dari Pinheiro <contatodariph27@gmail.com>
2026-05-13 12:15:14 -0300

    Resolvendo o problema do console preso

--- repositorio-teste-main/src/main.js
+++ repositorio-teste-main/src/main.js
@@ -20,43 +20,45 @@ async function main() {
     "Digite o segundo número: \n",
   ); // string -> texto-
 
-  const a = Number(aString);
-  if (isNaN(a)) {
-    throw new Error("O primeiro valor digitado não é um número válido.");
-  }
-
-  const b = Number(bString);
-  if (isNaN(b)) {
-    throw new Error("O segundo valor digitado não é um número válido.");
-  }
-
   // 1- Fazer a transformação para número -> Caso o usuário não digite um número, jogue um erro
   // 2- Criar as outras operações uma em cada arquivo e importar
   // BÔNUS: Resolver o problema do console preso quando a aplicação dá erro.
 
-  switch (respostaOperação) {
-    case "+":
-      const respostaAdicao = adicao(a, b);
-      console.log(`Resposta da operação: ${respostaAdicao}`);
-      break;
-    case "-":
-      const respostaSubtracao = subtracao(a, b);
-      console.log(`Resposta da operação: ${respostaSubtracao}`);
-      break;
-    case "/":
-      const respostaDivisao = divisao(a, b);
-      console.log(`Resposta da operação: ${respostaDivisao}`);
-      break;
-    case "*":
-      const respostaMultiplicacao = multiplicacao(a, b);
-      console.log(`Resposta da operação: ${respostaMultiplicacao}`);
-      break;
+  try {
+    const a = Number(aString);
+    if (isNaN(a)) {
+      throw new Error("O primeiro valor digitado não é um número válido.");
+    }
 
-    default:
-      throw new Error(`Não suportamos essa operação`);
+    const b = Number(bString);
+    if (isNaN(b)) {
+      throw new Error("O segundo valor digitado não é um número válido.");
+    }
+    switch (respostaOperação) {
+      case "+":
+        const respostaAdicao = adicao(a, b);
+        console.log(`Resposta da operação: ${respostaAdicao}`);
+        break;
+      case "-":
+        const respostaSubtracao = subtracao(a, b);
+        console.log(`Resposta da operação: ${respostaSubtracao}`);
+        break;
+      case "/":
+        const respostaDivisao = divisao(a, b);
+        console.log(`Resposta da operação: ${respostaDivisao}`);
+        break;
+      case "*":
+        const respostaMultiplicacao = multiplicacao(a, b);
+        console.log(`Resposta da operação: ${respostaMultiplicacao}`);
+        break;
+      default:
+        throw new Error(`Não suportamos essa operação`);
+    }
+  } catch (erro) {
+    console.error(erro.message);
+  } finally {
+    interfaceConsole.close();
   }
-
-  interfaceConsole.close();
 }
 
 main().catch(console.log);
